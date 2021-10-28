991dcc95f2f04062b9999d471b79d9d639c13dba
mateoavila <mateo.avila313@gmail.com>
2021-10-28 00:40:40 -0700

    final

--- src/matrixMult/matrixMult.java
+++ src/matrixMult/matrixMult.java
@@ -10,37 +10,47 @@ public class matrixMult {
 	public static void main(String[] args) {
 
 		String filepath = "MatrixMultResults.csv";
-		
+
 		long start;
 		long end;
-
+		
+		long cTotal = 0;
+		long dacTotal = 0;
+		long sTotal = 0;
+		long cCount = 0;
+		long dacCount = 0;
+		long sCount = 0;
+		long cAvg;
+		long dacAvg;
+		long sAvg;
+		
 		int dataSets = 200; // how many nxn matrixes will be made
 		int e = 9; // highest exponent of 2 to go to
-		int loop = 10; //how many times each matrix will be ran
-		
+		int loop = 10; // how many times each matrix will be ran
+
 		Random rand = new Random();
-		
+
 		title("size", "Classic", "Divide and Conquer", "Strassen", filepath);
 
-		
 		// increases the exponent from 1 to desired number
 		for (int exponent = 1; exponent <= e; exponent++) {
 
 			int size = (int) Math.pow(2, exponent);
 
-			long cTotal = 0;
-			long dacTotal = 0;
-			long sTotal = 0;
+			cTotal = 0;
+			dacTotal = 0;
+			sTotal = 0;
 
 			int[][] matrix1 = new int[size][size];
 			int[][] matrix2 = new int[size][size];
 
 			System.out.println("Array Size: " + size + "x" + size);
+
 			// loops for amount of data sets
 			for (int i = 0; i < dataSets; i++) {
-				long cCount = 0;
-				long dacCount = 0;
-				long sCount = 0;
+				cCount = 0;
+				dacCount = 0;
+				sCount = 0;
 
 				// fill matrix1 with random numbers;
 				for (int j = 0; j < matrix1.length; j++) {
@@ -90,59 +100,60 @@ public class matrixMult {
 				sTotal = sTotal + (sCount / loop);
 
 			}
-			
-			long cAvg = cTotal / dataSets;
-			long dacAvg = dacTotal / dataSets;
-			long sAvg = sTotal / dataSets;
-			
+
+			cAvg = cTotal / dataSets;
+			dacAvg = dacTotal / dataSets;
+			sAvg = sTotal / dataSets;
+
 			System.out.println("\nData Sets: " + dataSets);
 			System.out.println("Array Size: " + size + "x" + size);
 			System.out.println("Classic Mult average: 		 " + cAvg + " nanoseconds");
 			System.out.println("Divide and Conquer Mult average: " + dacAvg + " nanoseconds");
 			System.out.println("Strassen Mult average: 		 " + sAvg + " nanoseconds\n\n");
-			
+
 			saveRecord(size, cAvg, dacAvg, sAvg, filepath);
-			
+
 		}
 
 		System.out.println("\nDONE");
 
 	}
+
 	
 	
 	
 	public static void saveRecord(int matrixSize, long cAvg, long dacAvg, long sAvg, String filepath) {
-		
+
 		try {
 			FileWriter fw = new FileWriter(filepath, true);
 			BufferedWriter bw = new BufferedWriter(fw);
 			PrintWriter pw = new PrintWriter(bw);
-			
-			pw.println(matrixSize + "," + cAvg + "," +  dacAvg + "," + sAvg);
+
+			pw.println(matrixSize + "," + cAvg + "," + dacAvg + "," + sAvg);
 			pw.flush();
 			pw.close();
-			
-		}catch(Exception E){
-			
+
+		} catch (Exception E) {
+
 		}
-		
+
 	}
-	
-public static void title(String matrixSize, String cAvg, String dacAvg, String sAvg, String filepath) {
-		
+
+	public static void title(String matrixSize, String cAvg, String dacAvg, String sAvg, String filepath) {
+
 		try {
 			FileWriter fw = new FileWriter(filepath, true);
 			BufferedWriter bw = new BufferedWriter(fw);
 			PrintWriter pw = new PrintWriter(bw);
-			
-			pw.println(matrixSize + "," + cAvg + "," +  dacAvg + "," + sAvg);
+
+			pw.println(matrixSize + "," + cAvg + "," + dacAvg + "," + sAvg);
 			pw.flush();
 			pw.close();
-			
-		}catch(Exception E){
-			
+
+		} catch (Exception E) {
+
 		}
-		
+
 	}
 
 }
